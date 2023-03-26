e3f4b416d5e5e5c9327be2e50e8fab0316eec1ee
vladiazouga <vzouga2021@fau.edu>
2023-03-25 23:37:51 -0400

    adding the react tutorial package

---
+++ src/App.js
@@ -0,0 +1,27 @@
+function Square() {
+  return <button className="square">1</button>;
+}
+export default function Board() {
+  //This is a function component
+  return (
+    <>
+    <div className="board-row">
+    <button className="square">1</button>
+    <button className="square">2</button>
+    <button className="square">3</button>
+    </div>
+    <div className="board-row">
+    <button className="square">4</button>
+    <button className="square">5</button>
+    <button className="square">6</button>
+    </div>
+    <div className="board-row">
+    <button className="square">7</button>
+    <button className="square">8</button>
+    <button className="square">9</button>
+    </div>
+      
+    </>
+  );
+}
+
